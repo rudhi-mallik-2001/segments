00eae3c364f213f69405a0dc3edff2a33b85d55c
rudhi-mallik-2001 <120095736+rudhi-mallik-2001@users.noreply.github.com>
2025-10-26 23:54:41 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
+
+const WEBHOOKID=`49485054-0efc-4932-8cfa-b510745edb85`
 export const sendSegmentData = async (data) => {
   try {
-    const res = await fetch("/api/49485054-0efc-4932-8cfa-b510745edb85", {
+    const res = await fetch(`/api/${WEBHOOKID}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
